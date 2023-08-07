2cb416adc4250f265fb5123edd15b7b34c2ef15c
Avatar Tas <abdullahomar19971201@gmail.com>
2023-08-07 23:14:16 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import React, { useEffect, useState } from 'react';
+import { Login, Recipe } from "./screens";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Tabs from "./navigation/tabs";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+
+    const [fontLoaded, setFontLoaded] = useState(false);
+
+    useEffect(() => {
+        // Load the custom font asynchronously
+        const loadFont = async () => {
+          await Font.loadAsync({
+            'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+            'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf'),
+            'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+            // Add more custom fonts here if needed
+          });
+          setFontLoaded(true);
+        };
+    
+        loadFont();
+      }, []);
+
+      if (!fontLoaded) {
+        // Return a loading screen or null while the font is loading
+        return null;
+      }
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'Login'}
+            >
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                />
+                <Stack.Screen
+                    name="Home"
+                    component={Tabs}
+                />
+                <Stack.Screen
+                    name="Recipe"
+                    component={Recipe}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
